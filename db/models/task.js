cd8f4d6496435697817a75942b36032caf1441d9
Jason <jason.miguel@gmail.com>
2017-05-06 21:05:50 -0400

    added association for assignee and creator

--- db/models/task.js
+++ db/models/task.js
@@ -5,11 +5,11 @@ const { STRING } = require('sequelize');
 module.exports = db =>
   db.define('tasks', {
     description: STRING,
-    categories: STRING,
-    creator: STRING,
-    assignee: STRING
+    categories: STRING
   });
 
 module.exports.associations = (Bounty, { Task, BountyTask }) => {
   Task.belongsToMany(Bounty, { through: BountyTask });
+  Task.belongsTo(User, { as: 'creator' });
+  Task.belongsTo(User, { as: 'assignee' });
 };
