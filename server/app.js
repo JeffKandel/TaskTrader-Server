ab69865e4ecd7c0b9ad1b30b8d47cfcaf2dd9f43
Student <jeff.kandel@gmail.com>
2017-05-08 11:24:58 -0400

    feat: route all to graphQL and add logging

--- server/app.js
+++ server/app.js
@@ -1,20 +1,32 @@
-var express = require('express')
-var graphqlHTTP = require('express-graphql')
-var { buildSchema } = require('graphql')
+const express = require('express')
+const bodyParser = require('body-parser')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+const PrettyError = require('pretty-error')
 
-var schema = buildSchema(`
+
+if (!process.env.IS_PRODUCTION) {
+  // Logging middleware (dev only)
+  app.use(require('volleyball'))
+}
+
+const schema = buildSchema(`
   type Query {
     hello: String
   }
 `)
 
-var root = { hello: () => 'Hello world!' }
+const root = { hello: () => 'Hello world!' }
 
-var app = express()
-app.use('/graphql', graphqlHTTP({
+const app = express()
+
+app.use('/*', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
-}))
+})).use((err, req, res, next) => {
+  console.error(prettyError.render(err))
+  finalHandler(req, res)(err)
+})
 
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'))
