307064d05432170ab59aacd223c77f8f8a2271c0
Jason <jason.miguel@gmail.com>
2017-05-12 12:36:32 -0400

    temp: removing data history

--- db/models/task.js
+++ db/models/task.js
@@ -8,7 +8,8 @@ module.exports = db =>
       type: STRING,
       allowNull: false
     },
-    status: { type: STRING
+    status: {
+      type: STRING,
       defaultValue: 'pending'
     }
   });
