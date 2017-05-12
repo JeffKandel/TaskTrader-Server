1aa09a8373e1992d7fac4dc4b618e1c70178a00f
Daniel Fattal <dffattal@gmail.com>
2017-05-12 17:12:08 -0400

    feat: add bounty hook

--- db/models/bounty.js
+++ db/models/bounty.js
@@ -5,6 +5,12 @@ const { INTEGER } = require('sequelize');
 module.exports = db =>
   db.define('bounties', {
     amount: INTEGER
+  }, {
+    hooks: {
+      afterCreate: function(createdBounty) {
+        console.log(createdBounty[0].getTasks({include: {model: 'Task'}}))
+      }
+    }
   });
 
 module.exports.associations = (Bounty, { Task, BountyTask }) => {
